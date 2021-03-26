21e66345e4f5ae4ed169b848310d3b897c1e2590
Silambarasan N <silambarasan.n@atop.in>
2021-03-26 12:22:58 +0530

    First commit - Adding configuring collections and loading types

---
+++ HibernateFirst/Configuring Easy and eager fetching/dto/UserDetails.java
@@ -0,0 +1,64 @@
+package org.siimbu.java.dto;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CollectionId;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+@Entity
+@Table(name = "USER_DETAILS")
+public class UserDetails {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int userId;
+	private String userName;
+
+	@ElementCollection(fetch = FetchType.EAGER)
+	@JoinTable(name = "USER_ADDRESS", joinColumns = @JoinColumn(name = "USER_ID"))
+
+	private Collection<Address> listOfAddresses = new ArrayList<Address>();
+
+	public Collection<Address> getListOfAddresses() {
+		return listOfAddresses;
+	}
+
+	public void setListOfAddresses(Collection<Address> listOfAddresses) {
+		this.listOfAddresses = listOfAddresses;
+	}
+
+	public void setListOfAddresses(Set<Address> listOfAddresses) {
+		this.listOfAddresses = listOfAddresses;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+}
---
+++ HibernateFirst/Configuring Easy and eager fetching/hibernate/HibernateTest.java
@@ -0,0 +1,49 @@
+package org.siimbu.java.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.siimbu.java.dto.Address;
+import org.siimbu.java.dto.UserDetails;
+
+/**
+ * @author silambarasan
+ *
+ */
+public class HibernateTest {
+	public static void main(String[] args) {
+
+		UserDetails user = new UserDetails();
+		user.setUserName("First User");
+
+		Address addr1 = new Address();
+		addr1.setStreet("First Name");
+		addr1.setCity("City Name");
+		addr1.setState("First State");
+		addr1.setPincode("560043");
+
+		Address addr2 = new Address();
+		addr2.setStreet("Second Street");
+		addr2.setCity("Second City");
+		addr2.setState("Second State");
+		addr2.setPincode("120002");
+
+		user.getListOfAddresses().add(addr1);
+		user.getListOfAddresses().add(addr2);
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(user);
+		session.getTransaction().commit();
+		session.close();
+
+		user = null;
+		session = sessionFactory.openSession();
+		user = session.get(UserDetails.class, 1);
+		// session.close(); will get lazy initialization exception, since it is getting
+		// a proxy
+		session.close();
+		System.out.println(user.getListOfAddresses().size());
+
+	}
+}
