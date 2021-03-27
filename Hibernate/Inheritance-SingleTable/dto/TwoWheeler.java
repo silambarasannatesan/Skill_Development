2c3413107e4d2fe069dba27d6951858b38739d25
Silambarasan N <silambarasanasn@gmail.com>
2021-03-27 22:09:37 +0530

    Added how to implement inheritance using singe table strategy - discriminatorconfig

---
+++ Hibernate/Inheritance-SingleTable/dto/TwoWheeler.java
@@ -0,0 +1,19 @@
+package org.siimbu.java.dto;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("Bike")
+public class TwoWheeler extends Vehicle {
+	private String steeringHandler;
+
+	public String getSteeringHandler() {
+		return steeringHandler;
+	}
+
+	public void setSteeringHandler(String steeringHandler) {
+		this.steeringHandler = steeringHandler;
+	}
+
+}
---
+++ Hibernate/Inheritance-SingleTable/dto/Vehicle.java
@@ -0,0 +1,36 @@
+package org.siimbu.java.dto;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "VEHICLE_TYPE", discriminatorType = DiscriminatorType.STRING)
+public class Vehicle {
+	@Id
+	@GeneratedValue
+	private int vehicleId;
+	private String vehiclename;
+
+	public int getVehicleId() {
+		return vehicleId;
+	}
+
+	public void setVehicleId(int vehicleId) {
+		this.vehicleId = vehicleId;
+	}
+
+	public String getVehiclename() {
+		return vehiclename;
+	}
+
+	public void setVehiclename(String vehiclename) {
+		this.vehiclename = vehiclename;
+	}
+
+}
