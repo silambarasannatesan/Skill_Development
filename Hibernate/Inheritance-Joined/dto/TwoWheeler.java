241192b8f4930a30adc4c90073637fb0f2d4f884
Silambarasan N <silambarasanasn@gmail.com>
2021-03-27 22:37:30 +0530

    Added how to implement inheritance using join class strategy

---
+++ Hibernate/Inheritance-Joined/dto/TwoWheeler.java
@@ -0,0 +1,18 @@
+package org.siimbu.java.dto;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+public class TwoWheeler extends Vehicle {
+	private String steeringHandler;
+
+	public String getSteeringHandler() {
+		return steeringHandler;
+	}
+
+	public void setSteeringHandler(String steeringHandler) {
+		this.steeringHandler = steeringHandler;
+	}
+
+}
