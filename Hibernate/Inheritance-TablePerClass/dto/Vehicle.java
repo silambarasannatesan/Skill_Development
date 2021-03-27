9d1f622144b4d0eb0dcc1a197c23f76232ca8c1f
Silambarasan N <silambarasanasn@gmail.com>
2021-03-27 22:24:15 +0530

    Added how to implement inheritance using singe table per class strategy

---
+++ Hibernate/Inheritance-TablePerClass/dto/Vehicle.java
@@ -0,0 +1,33 @@
+package org.siimbu.java.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class Vehicle {
+	@Id
+	@GeneratedValue
+	private int vehicleId;
+	private String vehiclename;
+
+	public int getVehicleId() {
+		return vehicleId;
+	}
+
+	public void setVehicleId(int vehicleId) {
+		this.vehicleId = vehicleId;
+	}
+
+	public String getVehiclename() {
+		return vehiclename;
+	}
+
+	public void setVehiclename(String vehiclename) {
+		this.vehiclename = vehiclename;
+	}
+
+}
