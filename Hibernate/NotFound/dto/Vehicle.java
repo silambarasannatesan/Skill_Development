8e202473247b3a3ae2baa53171143bfeee44a9c2
Silambarasan N <silambarasanasn@gmail.com>
2021-03-27 19:26:28 +0530

    Added association not found code

---
+++ Hibernate/NotFound/dto/Vehicle.java
@@ -0,0 +1,45 @@
+package org.siimbu.java.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
+
+@Entity
+public class Vehicle {
+	@Id
+	@GeneratedValue
+	private int vehicleId;
+	private String vehiclename;
+	@ManyToOne
+	@NotFound(action = NotFoundAction.IGNORE)
+	private UserDetails userDetails;
+
+	public UserDetails getUserDetails() {
+		return userDetails;
+	}
+
+	public void setUserDetails(UserDetails userDetails) {
+		this.userDetails = userDetails;
+	}
+
+	public int getVehicleId() {
+		return vehicleId;
+	}
+
+	public void setVehicleId(int vehicleId) {
+		this.vehicleId = vehicleId;
+	}
+
+	public String getVehiclename() {
+		return vehiclename;
+	}
+
+	public void setVehiclename(String vehiclename) {
+		this.vehiclename = vehiclename;
+	}
+
+}
